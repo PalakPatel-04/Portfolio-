0041d9c118a0ae5cb0e8cef8c6b3ec60264fc812
Palak Patel <114860110+PalakPatel-04@users.noreply.github.com>
2025-01-03 10:34:16 +0530

    Update DetailedProjects.jsx

--- src/Components/Project/DetailedProjects.jsx
+++ src/Components/Project/DetailedProjects.jsx
@@ -5,7 +5,7 @@ const DetailedProject = ({ setSelectedTab }) => {
     {
       number: 1,
       title: "PixSnapper",
-      description:"PixSnapper is an AI-powered image generation app built using the MERN stack (MongoDB, Express.js, React.js, Node.js). The app allows users to generate images based on text prompts, using open api. This helps in better understanding of integration of apis in real-time. Hepls me to integrate AI functionalities with a full-stack web application for a dynamic user experience.",
+      description:"PixSnapper is an AI-powered image generation app built using the MERN stack (MongoDB, Express.js, React.js, Node.js). The app allows users to generate images based on text prompts, using open api. This helps in better understanding of integration of apis in real-time.",
       button1: "Github",
       button1link: "#",
       button2: "Live Demo",
@@ -15,7 +15,7 @@ const DetailedProject = ({ setSelectedTab }) => {
     {
       number: 2,
       title: "Blood donor connect portal",
-      description:"The Blood Donor Connect Portal is a full-stack web application built using the MERN stack (MongoDB, Express.js, React.js, Node.js), designed to connect blood donors with recipients. The platform allows users to register, search for donors, and update their profiles. I implemented RESTful APIs for data exchange and integrated JWT authentication to secure user and admin logins. MongoDB was used to store and retrieve user data efficiently, ensuring a smooth and secure experience for all users. This project helped me refine my skills in full-stack development, API integration, and database management",
+      description:"The Blood Donor Connect Portal is a full-stack web application built using the MERN stack (MongoDB, Express.js, React.js, Node.js), designed to connect blood donors with recipients. The platform allows users to register, search for donors, and update their profiles. Used RESTful APIs for data exchange and integrated JWT authentication to secure user and admin logins.",
       button1: "Github",
       button1link: "https://github.com/PalakPatel-04/Blood-Donor-Connect-Portal-",
       button2: "Live Demo",
@@ -26,7 +26,7 @@ const DetailedProject = ({ setSelectedTab }) => {
       number: 3,
       title:"Portfolio",
       description:
-        "The Portfolio is a personal project that I developed to showcase my skills, accomplishments, and the projects I have worked on. It features sections such as an about me, skills, projects, and contact information, providing a comprehensive overview of my technical journey. This project reflects my commitment to continuous improvement and serves as a platform to display my work and achievements.",
+        "The Portfolio is a personal project that I developed to showcase my skills, accomplishments, and the projects I have worked on.",
       button1: "Github",
       button1link: "#",
       button2: "Live Demo",
@@ -38,7 +38,7 @@ const DetailedProject = ({ setSelectedTab }) => {
     {
       number: 2,
       title: "LangDetect",
-      description: "LangDetect is a Python-based language detection project that utilizes pandas for data handling and scikit-learn for feature extraction. The project uses the Multinomial Naive Bayes Classifier to accurately classify text input by language. ",
+      description: "LangDetect is a Python-based language detection project that utilizes pandas for data handling and scikit-learn for feature extraction. Also Multinomial Naive Bayes Classifier to accurately classify text input by language. ",
       button1: "Github",
       button1link: "https://github.com/PalakPatel-04/LangDetect",
       button2: "GitHub Demo",
@@ -48,7 +48,7 @@ const DetailedProject = ({ setSelectedTab }) => {
     {
       number: 3,
       title: "ShopEase",
-      description: "ShopEase, an eCommerce website was developed as part of a college competition using basic HTML and CSS.The project helped me refine my skills in web designing, focusing on creating a clean, user-friendly interface.",
+      description: "ShopEase, an eCommerce website developed using basic HTML and CSS.",
       button1: "Github",
       button1link:"https://github.com/PalakPatel-04/ShopEase",
       button2: "Live Demo",
